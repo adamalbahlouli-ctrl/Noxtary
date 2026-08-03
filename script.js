ded07c63c6dfde5d43b1a6af18347cf2f172571e
Adam <adamalbahlouli@gmail.com>
2026-08-03 20:47:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1571,7 +1571,7 @@ async function signInWithGoogle() {
     const { data, error } = await supabaseClient.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: window.location.href
+            redirectTo: window.location.origin + '/'
         }
     });
     if (error) {
